27a67153518b5159c23677d6ca2d37a36003522e
Zhakupbekov Sayan <sake_1011@mail.ru>
2017-02-08 01:42:37 +0600

    Добавка

--- homework/lesson1-components/sayan.zhakupbekov/components/src/app/app.component.ts
+++ homework/lesson1-components/sayan.zhakupbekov/components/src/app/app.component.ts
@@ -106,6 +106,7 @@ const users = [
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   public title: string = 'Карточка участников курса';
   public users: Array<any> = users;
