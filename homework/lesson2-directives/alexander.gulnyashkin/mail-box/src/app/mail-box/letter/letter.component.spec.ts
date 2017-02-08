1c1faf3c904a6a8ac171f516b6206afe549de9a1
gulalex181 <gulalex181@gmail.com>
2017-02-08 19:31:46 +0300

    init

---
+++ homework/lesson2-directives/alexander.gulnyashkin/mail-box/src/app/mail-box/letter/letter.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { LetterComponent } from './letter.component';
+
+describe('LetterComponent', () => {
+  let component: LetterComponent;
+  let fixture: ComponentFixture<LetterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LetterComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LetterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
