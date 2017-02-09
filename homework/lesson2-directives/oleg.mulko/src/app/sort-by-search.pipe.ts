1e5241ef49f4b292db2dd96be3825c92c6d77156
Oleg Mulko <oleg.mylko@mail.ru>
2017-02-09 10:17:19 +0300

    Homework_2_mulko2

--- homework/lesson2-directives/oleg.mulko/src/app/sort-by-search.pipe.ts
+++ homework/lesson2-directives/oleg.mulko/src/app/sort-by-search.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SortBySearchPipe implements PipeTransform {
 
   transform(value: any, searchStr: string = ""): any {
-    let result: Array<string>;
     if (searchStr != "") {
       let sortData: Array<any> =  value.reduce((sortData, data) => {
         for (let key in data) {
