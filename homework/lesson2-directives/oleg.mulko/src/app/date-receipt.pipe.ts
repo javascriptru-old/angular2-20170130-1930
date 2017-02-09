c9fd3ec78589dbf44b3e0a34a65ffc7f809b3d95
Oleg Mulko <oleg.mylko@mail.ru>
2017-02-09 10:12:58 +0300

    Homework_2_mulko2

--- homework/lesson2-directives/oleg.mulko/src/app/date-receipt.pipe.ts
+++ homework/lesson2-directives/oleg.mulko/src/app/date-receipt.pipe.ts
@@ -5,40 +5,97 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateReceiptPipe implements PipeTransform {
 
-  transform(value: string, args?: any): string {
+  transform(value: string, dateFormat: string = "default"): string {
     let receiptDate: Date = new Date(Date.parse(value));
-    let date: number = new Date().getTime() - receiptDate.getTime();
+    let time: number = new Date().getTime() - receiptDate.getTime();
     let amountTime = {
+      sec: 1000,
       min : 60 * 1000,
       hour : 60 * 60 * 1000,
       day : 24 * 60 * 60 * 1000,
-      week : 7 * 24 * 60 * 60 * 1000
+      week : 7 * 24 * 60 * 60 * 1000,
+      month: 30 * 24 * 60 * 60 * 1000,
+      year: 365 * 24 * 60 * 60 * 1000
     };
+    if (dateFormat === "auto") {
+      let getDateFormat = () => {
+        if (time < amountTime.min) {
+          return "seconds";
+        }
 
-    if (date < amountTime.min) {
-      return `${new Date(date).getSeconds()} seconds ago!`;
+        if (time < amountTime.hour) {
+          return "minutes";
+        }
+
+        if (time < amountTime.day) {
+          return "hours";
+        }
+
+        if (time < amountTime.week) {
+          return "days";
+        }
+        return "default";
+      }
+      dateFormat = getDateFormat();
     }
-
-    if (date < amountTime.hour) {
-      return `${new Date(date).getMinutes()} minutes ago!`;
-    }
-
-    if (date < amountTime.day) {
-      return `${new Date(date).getHours()} hours ago!`;
-    }
-
-    if (date < amountTime.week) {
-      let days: number = date/amountTime.day;
-      return `${days^0} days ago!`;
-    }
-
-    var options = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    };
-
-    return receiptDate.toLocaleString("ru", options);
+      switch (dateFormat) {
+        case "seconds" : {
+          if (time >  amountTime.sec) {
+            let seconds: number = time/amountTime.sec;
+            return `${seconds} seconds ago!`;
+          }
+          return "0 seconds ago!";
+        }
+        case "minutes" : {
+          if (time >  amountTime.min){
+            let minutes: number = time/amountTime.min;
+            return `${minutes^0} minutes ago!`;
+          }
+          return "0 minutes ago!";
+        }
+        case "hours" : {
+          if (time >  amountTime.hour){
+            let hours: number = time/amountTime.hour;
+            return `${hours^0} days ago!`;
+          }
+          return "0 days ago!";
+        }
+        case "days" : {
+          if (time >  amountTime.day){
+            let days: number = time/amountTime.day;
+            return `${days^0} days ago!`;
+          }
+         return "0 days ago!"
+        }
+        case "weeks" : {
+          if (time >  amountTime.week){
+            let weeks: number = time/amountTime.week;
+            return `${weeks^0} weeks ago!`;
+          }
+          return "0 weeks ago!"
+        }
+        case "months" : {
+          if (time >  amountTime.month){
+            let months: number = time/amountTime.month;
+            return `${months^0} months ago!`;
+          }
+          return "0 months ago!"
+        }
+        case "years" : {
+          if (time >  amountTime.month){
+            let year: number = time/amountTime.year;
+            return `${year^0} years ago!`;
+          }
+          return "0 years ago!"
+        }
+        default : {
+          var options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+          };
+          return receiptDate.toLocaleString("ru", options);
+        }
+      }
   }
-
 }
--- homework/lesson2-directives/oleg.mulko/src/app/sort-by-search.pipe.ts
+++ homework/lesson2-directives/oleg.mulko/src/app/sort-by-search.pipe.ts
@@ -5,24 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortBySearchPipe implements PipeTransform {
 
-  transform(value: any, searchStr: string): any {
+  transform(value: any, searchStr: string = ""): any {
+    let result: Array<string>;
+    if (searchStr != "") {
       let sortData: Array<any> =  value.reduce((sortData, data) => {
         for (let key in data) {
           if (key === "date") {
             return sortData;
           }
           if (data[key].indexOf(searchStr) != -1) {
-           sortData.push(data);
+            sortData.push(data);
             return sortData;
           }
         }
         return sortData;
       }, []);
-    if (sortData.length != 0) {
       return sortData;
     }
-    else {
       return value;
-    }
   }
 }
